ffcbeba3c12d7a0e008c5bac94cb46e678c9943e
damian <damiakuz@wp.pl>
2019-07-20 11:06:45 +0200

    init

---
+++ SklepAsp/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SklepAsp.Models
+{
+    public class Product
+    {
+        [Key]
+        public string ProductId { get; set; }
+        public string Name { get; set; }
+        public Decimal Cost { get; set; }
+        public string Description { get; set; }
+        public DateTime DateCreated { get; set; }
+        public byte[] Photo { get; set; }
+    }
+}
